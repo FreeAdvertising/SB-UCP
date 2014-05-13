71b13259c9e216097e5fd006ab3f606220ff1126
Ryan Priebe <ryan@wearefree.ca>
2014-05-13 16:50:21 -0600

    Added Wordpress version checking to index.php

--- index.php
+++ index.php
@@ -3,14 +3,29 @@
  * Plugin Name: Super Badass UCP
  * Plugin URI: http://wearefree.ca
  * Description: User-group/level based controls for WP's backend
- * Version: 1.0
+ * Version: 1.0.0
  * Author: Free Advertising
  * Author URI: http://wearefree.ca
  * License: BSD
  */
 
-include 'usercontrol.php';
+define("SBUCP_MIN_WP_VERSION", "3.5.0");
 
-$widget = new \Free\SB_UserControl();
+if(version_compare($wp_version, SBUCP_MIN_WP_VERSION) === 1){
+	include 'usercontrol.php';
+
+	$widget = new \Free\SB_UserControl();
+}else {
+	//unsupported WP version, display an error
+	add_action("admin_notices", function(){
+		global $wp_version;
+
+		echo sprintf('
+			<div class="error">
+				<p><strong>SB-UCP Error</strong>: Your version of Wordpress (%s) is not supported.</p>
+			</div>
+		', $wp_version);
+	});
+}
 
 ?>
